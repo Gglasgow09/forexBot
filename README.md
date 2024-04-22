bc8cd4c859ab4bca0abf5406021e2f5de31eac3c
Gabrielle <102875018+Gglasgow09@users.noreply.github.com>
2024-04-22 00:44:35 -0400

    added in session

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # forexBot
 
 used postman for api response requests 
-using python to create a forex bot and backtest system
+using python to create a forex bot and backtest system 
--- defs.py
+++ defs.py
@@ -3,5 +3,5 @@ ACCOUNT_ID = "101-001-28943203-001"
 OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
 
 SECURE_HEADER = {
-    'Authorization': f'Bearer{API_KEY}'
+    'Authorization' : f'Bearer {API_KEY}'
 }
