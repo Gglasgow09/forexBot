7af06b4cc7fbedcb822e7276270c11c6990ab6c0
Gabrielle <102875018+Gglasgow09@users.noreply.github.com>
2024-04-22 00:04:09 -0400

    created defs.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # forexBot
+
+used postman for api response requests 
+using python to create a forex bot and backtest system
---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "a278753502a4c8f8a50b8937b1232295-a446a3702d58ed54691265b26759d511"
+ACCOUNT_ID = "101-001-28943203-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer{API_KEY}'
+}
