7482a7fc273e851459547132ac899464162c7b31
Gabrielle <102875018+Gglasgow09@users.noreply.github.com>
2024-05-01 23:39:05 -0400

    completed fetch candle function in oandaAPI

--- oanda_api.py
+++ oanda_api.py
@@ -14,8 +14,18 @@ class OandaAPI():
         pass
     def save_instruments(self):
         pass
-    def save_candles(self, pair_name, count, granularity):
+    def fetch_candles(self, pair_name, count, granularity):
         url = f"{defs.OANDA_URL}/instruments/{pair_name}/candles"
 
+        params = dict(
+            count = count,
+            granularity = granularity,
+            price = "MBA"
+        )
+
+        response =  self.session.get(url, params=params, headers=defs.SECURE_HEADER)
+        return response.status_code, response.json()
+
 if __name__ == "__main__":
-    api = OandaAPI
+    api = OandaAPI()
+    print(api.fetch_candles("EUR_NOK", 50, "H4"))
